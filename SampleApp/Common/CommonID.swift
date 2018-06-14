fc04617ce1a081a40561a6c08e51c0918ab1e412
Nikki Hu <hujhh@neusoft.co.jp>
2018-06-14 15:29:26 +0900

    Added Common functions

---
+++ SampleApp/Common/CommonID.swift
@@ -0,0 +1,23 @@
+//
+//  CommonID.swift
+//  SampleApp
+//
+//  Created by Nikki on 2018/06/14.
+//  Copyright © 2018年 Nikki. All rights reserved.
+//
+
+import Foundation
+
+struct SettingID {
+    static let DID_SIGNIN = "DID_SIGNIN"
+    static let USER_REGISTER_DATA = "USER_REGISTER_DATA"
+}
+
+struct UserDataID {
+    static let USERID = "USERID"
+    static let IDTOKEN = "IDTOKEN"
+    static let GIVEN_NAME = "GIVEN_NAME"
+    static let FAMILY_NAME = "FAMILY_NAME"
+    static let EMAIL = "EMAIL"
+    static let USER_IMAGE = "USER_IMAGE"
+}
