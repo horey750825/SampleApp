be7439eec7e0dfa36f026f2aaa9271808f401c43
Nikki Hu <hujhh@neusoft.co.jp>
2018-06-14 17:50:29 +0900

    work temp 0614 - 2

--- SampleApp/ViewController/FirstViewController.swift
+++ SampleApp/ViewController/FirstViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,7 +33,18 @@ class FirstViewController: UIViewController {
     }
     
     func prepareSetting() {
-        
+        setLabel()
+    }
+    
+    func setLabel() {
+        if let userData = CommonManager.sharedInstance.ud.dictionary(forKey: SettingID.USER_REGISTER_DATA) {
+            var userName = userData[UserDataID.GIVEN_NAME] as! String
+            userName += " "
+            userName += userData[UserDataID.FAMILY_NAME] as! String
+            userNameLabel.text = userName
+        } else {
+            userNameLabel.text = "Hi"
+        }
     }
 
     override func didReceiveMemoryWarning() {
