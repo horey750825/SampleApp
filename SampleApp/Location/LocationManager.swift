6618ee8d62946dcf76e4a068f5dc7375f2442928
Nikki Hu <hujhh@neusoft.co.jp>
2018-06-29 16:36:57 +0900

    Fixed the LocationManger bug
    https://stackoverflow.com/questions/10707038/location-manager-was-created-on-a-dispatch-queue

--- SampleApp/Location/LocationManager.swift
+++ SampleApp/Location/LocationManager.swift
@@ -14,13 +14,21 @@ public protocol LocationManagerDelegate {
 
 class LocationManager: NSObject, CLLocationManagerDelegate {
     static let sharedInstance : LocationManager = LocationManager()
-    let manager = CLLocationManager()
+    var manager : CLLocationManager!
     var delegate : LocationManagerDelegate?
+    
+    var currentLocation : CLLocation!
     
     var didCheckAuthorization = false
     
     private override init() {
         super.init()
+        DispatchQueue.main.async {
+            self.manager = CLLocationManager()
+            self.manager.distanceFilter = kCLLocationAccuracyNearestTenMeters
+            self.manager.desiredAccuracy = kCLLocationAccuracyBest
+            self.manager.delegate = self
+        }
     }
     
     func getAuthorize() {
@@ -28,14 +36,32 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         
         switch status {
         case .authorizedWhenInUse:
+            self.startUpdatingLocation()
             return
         case .denied, .restricted:
             logger.debug("request denied")
+            self.stopUpdatingLocation()
         default:
             manager.requestWhenInUseAuthorization()
             didCheckAuthorization = true
         }
     }
     
+    func stopUpdatingLocation() {
+        DispatchQueue.main.async {
+            self.manager.stopUpdatingLocation()
+        }
+    }
+    
+    func startUpdatingLocation() {
+        DispatchQueue.main.async {
+            self.manager.startUpdatingLocation()
+        }
+    }
+    
     // MARK CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.currentLocation = locations[0]
+        logger.debug("location \(self.currentLocation.coordinate.latitude), \(self.currentLocation.coordinate.longitude)")
+    }
 }
