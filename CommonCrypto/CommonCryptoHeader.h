64224373e03ccc5dbc2d022a47cea994c252aad1
NIkki <horey750825@gmail.com>
2018-07-04 23:39:00 +0900

    Imported the CommonCrypto to project

---
+++ CommonCrypto/CommonCryptoHeader.h
@@ -0,0 +1,14 @@
+//
+//  CommonCryptoHeader.h
+//  SampleApp
+//
+//  Created by Nikki on 2018/7/4.
+//  Copyright © 2018年 Nikki. All rights reserved.
+//
+
+#ifndef CommonCryptoHeader_h
+#define CommonCryptoHeader_h
+
+#include <CommonCrypto/CommonCrypto.h>
+
+#endif /* CommonCryptoHeader_h */
