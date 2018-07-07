aacc741e639a7f75b0cc2e0b64fe8400e869f464
NIkki <horey750825@gmail.com>
2018-07-07 15:29:42 +0900

    Added check network function

--- SampleApp/Common/Common.swift
+++ SampleApp/Common/Common.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import CommonCrypto
+import SystemConfiguration
 
 enum AuthResult {
     case success(Bool), failure(Error?)
@@ -25,9 +26,15 @@ struct Common {
     static let USERDATA_FAMILY_NAME = "USERDATA_FAMILY_NAME"
     static let USERDATA_EMAIL = "USERDATA_EMAIL"
     static let USERDATA_USER_IMAGE = "USERDATA_USER_IMAGE"
+    static var didConnectNetwork = false
     
     static func showSimpleAlert(Title: String, Message: String) {
         let alert = UIAlertController(title: Title, message: Message)
+        alert.show()
+    }
+    
+    static func showNetworkWarningAlert() {
+        let alert = UIAlertController(title: "Warning", message: "Please check network")
         alert.show()
     }
     
@@ -59,7 +66,38 @@ struct Common {
         return newImage!
     }
     
-
+    static func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
+        }
+        
+        /* Only Working for WIFI
+         let isReachable = flags == .reachable
+         let needsConnection = flags == .connectionRequired
+         
+         return isReachable && !needsConnection
+         */
+        
+        // Working for Cellular and WIFI
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
+        
+        return ret
+        
+    }
 }
 
 struct DeviceProfile {
