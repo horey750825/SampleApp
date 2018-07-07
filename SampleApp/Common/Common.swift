104d00460f0cb51ab1435ec23fa6d758d598f9aa
NIkki <horey750825@gmail.com>
2018-07-07 14:51:37 +0900

    added some common functions.
    getDocumentsDirectory and resizeImage

--- SampleApp/Common/Common.swift
+++ SampleApp/Common/Common.swift
@@ -30,6 +30,36 @@ struct Common {
         let alert = UIAlertController(title: Title, message: Message)
         alert.show()
     }
+    
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    static func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
+
 }
 
 struct DeviceProfile {
